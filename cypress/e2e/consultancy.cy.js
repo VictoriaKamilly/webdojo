e4f3dcbfc653f08ec7878dfcb74406479d5f7404
Victoria Kamilly <155745312+VictoriaKamilly@users.noreply.github.com>
2026-01-29 11:58:01 -0300

    Improve consultancy form submission test
    Refactor consultancy form test to streamline checks and submissions.

--- cypress/e2e/consultancy.cy.js
+++ cypress/e2e/consultancy.cy.js
@@ -77,17 +77,52 @@ describe("Formulário de consultoria ", () => {
 
         const discoveryChannels = ['Instagram', 'LinkedIn', 'Udemy', 'YouTube', 'Indicação de Amigo'] //checkbox multiplos
 
-         discoveryChannels.forEach((channel) => {
+        discoveryChannels.forEach((channel) => {
             cy.contains('label', channel)
-            .find('input')
-            .check()
-            .should('be.checked')
-         });
+                .find('input')
+                .check()
+                .should('be.checked')
+        });
 
-         cy.get('input[type=file') //upload de arquivos 
-            .selectFile('./cypress/fixtures/relatorioPowerbanks.pdf', {force:true}) 
+        cy.get('input[type=file') //upload de arquivos 
+            .selectFile('./cypress/fixtures/relatorioPowerbanks.pdf', { force: true })
 
         cy.get('textArea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
-         .type('"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"')
+            .type('"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"')
+
+
+        const techs = [
+            'Cypress',
+            'Selenium',
+            'WebdriverIO',
+            'Playwright',
+            'Robot Framework'
+        ]
+
+        techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}') //dessa forma o cypress consegue ter o comportamento do usuário ao pressionar a tecla enter
+
+
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
+        })
+
+        cy.contains('label', 'termos de uso')
+            .find('input')
+            .check()
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+            .should('be.visible')
+            .wait(3000)
+        
+        cy.contains('button', 'Fechar')
+            .click()
     });
 });
